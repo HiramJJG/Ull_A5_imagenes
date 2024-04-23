1c4e6f8d0407c965f42a0043b9e62cdf737e8acc
Hiram_Jurado <hiramjurado16@gmail.com>
2024-04-23 00:17:55 +0000

    Actividad 5

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Mostrar Imagenes Jurado Garcia '),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(
+          title,
+          style: TextStyle(color: Color(0xffffffff)),
+        ),
+        backgroundColor: Color(0xff07b507),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 250.0,
+            width: 450.0,
+            child: Image.asset(
+              "assets/serpiente.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 250.0,
+            width: 450.0,
+            child: Image.network(
+              "https://concepto.de/wp-content/uploads/2020/04/tortuga-e1588195737163-800x400.jpg",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
